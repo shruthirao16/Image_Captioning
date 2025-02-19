95db62fa0bbaf1f57f9ee36af731fd29c8011d50
tshruthirao <tshruthirao16@gmail.com>
2025-02-20 01:53:45 +0530

    Updated main.py

--- main.py
+++ main.py
@@ -85,11 +85,16 @@ def save_descriptions(descriptions, filename):
     file.close()
 
 # Set these path according to project folder in you system
-dataset_text = r"C:\Users\takka\OneDrive\Desktop\ALL PROJECTS\Image_captioning\Flickr8k_text"
-dataset_images = r"C:\Users\takka\OneDrive\Desktop\ALL PROJECTS\Image_captioning\Flickr8k_Dataset"
+dataset_text = r"C:\Users\takka\OneDrive\Desktop\PROJECTS\Image_captioning\Flickr8k_text"
+dataset_images = r"C:\Users\takka\OneDrive\Desktop\PROJECTS\Image_captioning\Flickr8k_Dataset"
 
 #we prepare our text data
-filename = dataset_text + "/" + "Flickr8k.token.txt"
+# filename = dataset_text + "/" + "Flickr8k.token.txt" 
+
+filename = r"C:\Users\takka\OneDrive\Desktop\PROJECTS\Image_captioning\Flickr8k_text\Flickr8k.token.txt"
+descriptions = all_img_captions(filename)
+
+
 #loading the file that contains all data
 #mapping them into descriptions dictionary img to 5 captions
 descriptions = all_img_captions(filename)
@@ -121,28 +126,290 @@ weights_url = "https://storage.googleapis.com/tensorflow/keras-applications/xcep
 weights_path = download_with_retry(weights_url, 'xception_weights.h5')
 model = Xception(include_top=False, pooling='avg', weights=weights_path)
 
-def extract_features(directory):
-    features = {}
-    valid_images = ['.jpg', '.jpeg', '.png']  # Add other formats if needed
-    
-    for img in tqdm(os.listdir(directory)):
-        # Skip files that don't end with valid image extensions
-        ext = os.path.splitext(img)[1].lower()
-        if ext not in valid_images:
-            continue
-            
-        filename = directory + "/" + img
-        image = Image.open(filename)
-        image = image.resize((299,299))
-        image = np.expand_dims(image, axis=0)
-        image = image/127.5
-        image = image - 1.0
 
-        feature = model.predict(image)
-        features[img] = feature
+
+from pickle import dump
+
+
+# Ensure your model is defined before running this
+# Example: model = YourPretrainedModel()
+
+# def extract_features(directory):
+#     features = {}
+#     valid_images = ['.jpg', '.jpeg', '.png']  # Valid image formats
+
+#     for img in tqdm(os.listdir(directory)):
+#         ext = os.path.splitext(img)[1].lower()
+#         if ext not in valid_images:
+#             continue  # Skip non-image files
+
+#         filename = os.path.join(directory, img)
+#         image = Image.open(filename).resize((299, 299))
+#         image = np.expand_dims(np.array(image) / 127.5 - 1.0, axis=0)
+
+#         feature = model.predict(image)  # Ensure 'model' is loaded
+#         features[img] = feature
+
+#     return features
+
+# dataset_images = r"C:\Users\takka\OneDrive\Desktop\PROJECTS\Image_captioning\Flickr8k_Dataset\Flicker8k_Dataset"  # Update the path
+# features = extract_features(dataset_images)
+
+# # Save features to file
+# dump(features, open("features.p", "wb"))
+# print("✅ Feature extraction completed and saved to features.p")
+
+
+
+
+
+
+
+# def extract_features(directory):
+#     features = {}
+#     valid_images = ['.jpg', '.jpeg', '.png']  # Add other formats if needed
+    
+#     for img in tqdm(os.listdir(directory)):
+#         # Skip files that don't end with valid image extensions
+#         ext = os.path.splitext(img)[1].lower()
+#         if ext not in valid_images:
+#             continue
+            
+#         filename = directory + "/" + img
+#         image = Image.open(filename)
+#         image = image.resize((299,299))
+#         image = np.expand_dims(image, axis=0)
+#         image = image/127.5
+#         image = image - 1.0
+
+#         feature = model.predict(image)
+#         features[img] = feature
+#     return features
+
+# # 2048 feature vector
+# features = extract_features(dataset_images)
+# dump(features, open("features.p","wb"))
+
+features = load(open("features.p","rb"))
+
+#load the data 
+def load_photos(filename):
+    file = load_doc(filename)
+    photos = file.split("\n")[:-1]
+
+    print(f"✅ Loaded train image list. Total images: {len(photos)}")
+
+    # Print first few image names
+    print("First 5 images:", photos[:5])
+
+    return photos
+
+
+
+
+def load_clean_descriptions(filename, photos): 
+    file = load_doc(filename)
+    print(f"✅ Loaded {filename}. Total lines: {len(file.splitlines())}")
+
+    descriptions = {}
+
+    for i, line in enumerate(file.split("\n")):
+        words = line.split("\t")  # Use tab (\t) as separator
+        if len(words) < 2:
+            continue
+
+        image, image_caption = words[0], words[1]  # No need to split on '#'
+
+        if image in photos:
+            if image not in descriptions:
+                descriptions[image] = []
+            desc = f"<start> {image_caption} <end>"
+            descriptions[image].append(desc)
+
+            if i < 5:  # Print first few processed descriptions
+                print(f"{i}: {image} -> {desc}")
+
+    print(f"✅ Total descriptions loaded: {len(descriptions)}")
+    return descriptions
+
+
+
+
+
+
+
+def load_features(photos):
+    #loading all features
+    all_features = load(open("features.p","rb"))
+    #selecting only needed features
+    features = {k:all_features[k] for k in photos}
     return features
 
-# 2048 feature vector
-features = extract_features(dataset_images)
-dump(features, open("features.p","wb"))
+
+filename = r"C:\Users\takka\OneDrive\Desktop\PROJECTS\Image_captioning\Flickr8k_text\Flickr_8k.trainImages.txt"
+
+print("Full path to train images file:", filename)
+
+
+#train = loading_data(filename)
+train_imgs = load_photos(filename)
+train_descriptions = load_clean_descriptions("descriptions.txt", train_imgs)
+train_features = load_features(train_imgs)
+
+
+#own
+print(f"Total train descriptions: {len(train_descriptions)}")
+print("First 5 descriptions:", list(train_descriptions.items())[:5])
+
+
+
+
+#converting dictionary to clean list of descriptions
+def dict_to_list(descriptions):
+    all_desc = []
+    for key in descriptions.keys():
+        [all_desc.append(d) for d in descriptions[key]]
+    return all_desc
+
+#creating tokenizer class 
+#this will vectorise text corpus
+#each integer will represent token in dictionary
+
+def create_tokenizer(descriptions):
+    desc_list = dict_to_list(descriptions)
+    tokenizer = Tokenizer()
+    tokenizer.fit_on_texts(desc_list)
+    return tokenizer
+
+# give each word an index, and store that into tokenizer.p pickle file
+tokenizer = create_tokenizer(train_descriptions)
+dump(tokenizer, open('tokenizer.p', 'wb'))
+vocab_size = len(tokenizer.word_index) + 1
+print(vocab_size)
+
+#calculate maximum length of descriptions
+def max_length(descriptions):
+    desc_list = dict_to_list(descriptions)
+    return max(len(d.split()) for d in desc_list)
+    
+max_length = max_length(train_descriptions)
+print(max_length)
+
+#create input-output sequence pairs from the image description.
+
+#data generator, used by model.fit()
+def data_generator(descriptions, features, tokenizer, max_length):
+    def generator():
+        while True:
+            for key, description_list in descriptions.items():
+                feature = features[key][0]
+                input_image, input_sequence, output_word = create_sequences(tokenizer, max_length, description_list, feature)
+                for i in range(len(input_image)):
+                    yield {'input_1': input_image[i], 'input_2': input_sequence[i]}, output_word[i]
+    
+    # Define the output signature for the generator
+    output_signature = (
+        {
+            'input_1': tf.TensorSpec(shape=(2048,), dtype=tf.float32),
+            'input_2': tf.TensorSpec(shape=(max_length,), dtype=tf.int32)
+        },
+        tf.TensorSpec(shape=(vocab_size,), dtype=tf.float32)
+    )
+    
+    # Create the dataset
+    dataset = tf.data.Dataset.from_generator(
+        generator,
+        output_signature=output_signature
+    )
+    
+    return dataset.batch(32)
+
+def create_sequences(tokenizer, max_length, desc_list, feature):
+    X1, X2, y = list(), list(), list()
+    # walk through each description for the image
+    for desc in desc_list:
+        # encode the sequence
+        seq = tokenizer.texts_to_sequences([desc])[0]
+        # split one sequence into multiple X,y pairs
+        for i in range(1, len(seq)):
+            # split into input and output pair
+            in_seq, out_seq = seq[:i], seq[i]
+            # pad input sequence
+            in_seq = pad_sequences([in_seq], maxlen=max_length)[0]
+            # encode output sequence
+            out_seq = to_categorical([out_seq], num_classes=vocab_size)[0]
+            # store
+            X1.append(feature)
+            X2.append(in_seq)
+            y.append(out_seq)
+    return np.array(X1), np.array(X2), np.array(y)
+
+#You can check the shape of the input and output for your model
+dataset = data_generator(train_descriptions, features, tokenizer, max_length)
+for (a, b) in dataset.take(1):
+    print(a['input_1'].shape, a['input_2'].shape, b.shape)
+    break
+
+from keras.utils import plot_model
+
+# define the captioning model
+def define_model(vocab_size, max_length):
+
+    # features from the CNN model squeezed from 2048 to 256 nodes
+    inputs1 = Input(shape=(2048,), name='input_1')
+    fe1 = Dropout(0.5)(inputs1)
+    fe2 = Dense(256, activation='relu')(fe1)
+
+    # LSTM sequence model
+    inputs2 = Input(shape=(max_length,), name='input_2')
+    se1 = Embedding(vocab_size, 256, mask_zero=True)(inputs2)
+    se2 = Dropout(0.5)(se1)
+    se3 = LSTM(256)(se2)
+
+    # Merging both models
+    decoder1 = add([fe2, se3])
+    decoder2 = Dense(256, activation='relu')(decoder1)
+    outputs = Dense(vocab_size, activation='softmax')(decoder2)
+
+    # tie it together [image, seq] [word]
+    model = Model(inputs=[inputs1, inputs2], outputs=outputs)
+    model.compile(loss='categorical_crossentropy', optimizer='adam')
+
+    # summarize model
+    print(model.summary())
+    plot_model(model, to_file='model.png', show_shapes=True)
+
+    return model
+
+# train our model
+print('Dataset: ', len(train_imgs))
+print('Descriptions: train=', len(train_descriptions))
+print('Photos: train=', len(train_features))
+print('Vocabulary Size:', vocab_size)
+print('Description Length: ', max_length)
+
+model = define_model(vocab_size, max_length)
+epochs = 10
+
+def get_steps_per_epoch(train_descriptions):
+    total_sequences = 0
+    for img_captions in train_descriptions.values():
+        for caption in img_captions:
+            words = caption.split()
+            total_sequences += len(words) - 1
+    # Ensure at least 1 step, even if sequences < batch_size
+    return max(1, total_sequences // 32)
+
+# Update training loop
+steps = get_steps_per_epoch(train_descriptions)
+
+# making a directory models to save our models
+os.mkdir("models2")
+for i in range(epochs):
+    dataset = data_generator(train_descriptions, train_features, tokenizer, max_length)
+    model.fit(dataset, epochs=4, steps_per_epoch=steps, verbose=1)
+    model.save("models2/model_" + str(i) + ".h5")
+
+
+
 
